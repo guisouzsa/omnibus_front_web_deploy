8c9ccf93ec219938f8f403fe87f36ad9c2b79c9e
gui <168765125+guisouzsa@users.noreply.github.com>
2026-05-07 21:57:42 -0300

    Configure remote image patterns in next.config.ts
    Added remotePatterns for image configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "omnibus.up.railway.app",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
